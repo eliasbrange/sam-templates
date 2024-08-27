bfd72688fe91db0b50404a85d0c2864fb38a6a11
Elias Brange <elias.brange@gmail.com>
2024-08-27 14:10:47 +0200

    fix: biome

--- nodejs/minimal/{{cookiecutter.project_name}}/src/functions/api.ts
+++ nodejs/minimal/{{cookiecutter.project_name}}/src/functions/api.ts
@@ -1,37 +1,37 @@
 import middy from "@middy/core";
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import {
-	captureLambdaHandler,
-	injectLambdaContext,
-	logMetrics,
-	logger,
-	metrics,
-	tracer,
+  captureLambdaHandler,
+  injectLambdaContext,
+  logMetrics,
+  logger,
+  metrics,
+  tracer,
 } from "../powertools";
 
 export const lambdaHandler = async (
-	event: APIGatewayProxyEvent,
+  event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-	logger.info("Received request", { event });
-	try {
-		return {
-			statusCode: 200,
-			body: JSON.stringify({
-				message: "hello world",
-			}),
-		};
-	} catch (err) {
-		console.log(err);
-		return {
-			statusCode: 500,
-			body: JSON.stringify({
-				message: "some error happened",
-			}),
-		};
-	}
+  logger.info("Received request", { event });
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "some error happened",
+      }),
+    };
+  }
 };
 
 export const handler = middy(lambdaHandler)
-	.use(injectLambdaContext(logger, { clearState: true }))
-	.use(logMetrics(metrics, { captureColdStartMetric: true }))
-	.use(captureLambdaHandler(tracer));
+  .use(injectLambdaContext(logger, { clearState: true }))
+  .use(logMetrics(metrics, { captureColdStartMetric: true }))
+  .use(captureLambdaHandler(tracer));
